ccd6bbc95a551d523ce63d13a70a2f8f8227eb7b
cielzhang <ciel.zhang@timerchina.com>
2016-12-08 10:34:09 +0800

    issue update

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="main-content">
+      <home></home>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+export default {
+  name: 'app',
+  data() {
+    return {
+    }
+  },
+  created: function() {
+    let vm = this;
+  },
+  components: {
+    Home,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Microsoft YaHei", "Verdana", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  position: relative;
+  min-width: 480px;
+  margin: 0;
+  font-size: 12px;
+}
+.main-content {
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <swiper class="my-swiper" :options="swiperOption">
+      <swiper-slide v-for="i in arr">Slide {{i}}</swiper-slide>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+import { swiper, swiperSlide, swiperPlugins } from 'vue-awesome-swiper'
+export default {
+  name: 'home',
+  created: function() {
+    let vm = this
+    setTimeout(function() {
+          vm.arr.push(4);
+          vm.arr.push(5);
+          vm.arr.push(6);
+    }, 5000);
+  },
+  data() {
+    return {
+      arr:[1,2,3],
+      swiperOption: {
+          nextButton: '.swiper-button-next',
+          prevButton: '.swiper-button-prev',
+          slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true
+      }
+    }
+  },
+  methods: {
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+<style scoped>
+.my-swiper {
+  text-align: center;
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
